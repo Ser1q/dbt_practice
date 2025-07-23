3c1fa65f8e5bd53753fe60507e9ec5269735a06f
Nuradil Serik <serik.nuradil@gmail.com>
2025-07-23 11:54:29 +0500

    stg flights incremental materialization add

--- models/staging/flights/stg_flights__flights.sql
+++ models/staging/flights/stg_flights__flights.sql
@@ -1,9 +1,11 @@
 {{
   config(
-    materialized = 'table'
+    materialized = 'incremental',
+    incremental_strategy = 'merge',
+    unique_key = ['flight_id']
     )
 }}
-select
+SELECT
   flight_id,
   flight_no,
   scheduled_departure,
@@ -15,5 +17,9 @@ select
   actual_departure,
   actual_arrival
 
-from {{ source('demo_src', 'flights') }}
+FROM {{ source('demo_src', 'flights') }}
+{% if is_incremental() %}
+WHERE
+  scheduled_departure > (SELECT MAX(scheduled_departure) FROM {{ source('demo_src', 'flights') }}) - INTERVAL '100 days'
+{% endif %}
     
